493bd3eaec63048b861515d89a710f61861224c1
Yusheng Wu <wuyusheng1023@gmail.com>
2020-01-20 16:43:54 +0200

    modified regex

--- app.py
+++ app.py
@@ -87,8 +87,9 @@ def parse_data(string):
     sep = CHARS['CR'] + CHARS['LF']
     str_arr = string.split(sep)
     str_arr = str_arr[0:-1] # delete last element (empty or incomplete)
+    print(str_arr)
     columns = ['Status', 'temperature', 'airflow', 'Detector1', 'Detector2', 'conductivity']
-    pattern = 'Y;\d{5}.\d{5};\d{2};\d{1};\d{2};[a-zA-Z0-9]{4};\d{4};\d{3};\d{5};\d{5};\d{5};\d{5};\d{5}'
+    pattern = 'Y;\d{5}.\d{5};\d{2};\d{1};\d{2};[a-zA-Z0-9]{4};\d{4};\d{3};\d{5};\d{5};.\d{4};.\d{4};.\d{4}'
     for s in str_arr:
         if s and s[0] == COMMANDS['Data'] and re.fullmatch(pattern, s): 
             x = s.split(';')
