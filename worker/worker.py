9aaea666458bf462aa77576b0ae216b330f67da9
Yusheng <wuyusheng1023@gmail.com>
2021-04-21 21:09:24 +0100

    KeyboardInterrupt

--- worker/worker.py
+++ worker/worker.py
@@ -1,19 +1,25 @@
-import configparser
+import sys, os, configparser
+from time import sleep
+from datetime import datetime
 
 import conf
 
 from airrmonia import Airrmonia
 from mfc import Mfc
-from mesayss import meassys
+from meassys import MeasSys
+
+from helpers import is_new_start
 
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
 AUTO_START = True if config['SETTINGS']['AUTO_START'] == 'True' else False
 AUTO_PUBLISH = True if config['SETTINGS']['AUTO_PUBLISH'] == 'True' else False
-AUTO_BACKUP = True if config['SETTINGS']['AUTO_PUBLISH'] == 'True' else False
-UPDATE_INTERVAL = int(config['SETTINGS']['AUTO_PUBLISH'])
-RECORD_INTERVAL = int(config['SETTINGS']['AUTO_PUBLISH'])
+AUTO_BACKUP = True if config['SETTINGS']['AUTO_BACKUP'] == 'True' else False
+LOCAL_PUBLISH_INTERVAL = int(config['SETTINGS']['LOCAL_PUBLISH_INTERVAL'])
+LOCAL_RECORD_INTERVAL = int(config['SETTINGS']['LOCAL_RECORD_INTERVAL'])
+REMOTE_PUBLISH_INTERVAL = int(config['SETTINGS']['REMOTE_PUBLISH_INTERVAL'])
+REMOTE_BACKUP_INTERVAL = int(config['SETTINGS']['REMOTE_BACKUP_INTERVAL'])
 
 airrmonia = Airrmonia(conf.PORT)
 mfc_sample = Mfc(
@@ -28,4 +34,39 @@ mfc_cal = Mfc(
 )
 meassys = MeasSys(airrmonia, mfc_sample, mfc_cal)
 
+if AUTO_START:
+  meassys.start()
 
+gen_local_pub = is_new_start(LOCAL_PUBLISH_INTERVAL)
+gen_local_record = is_new_start(LOCAL_RECORD_INTERVAL)
+gen_remote_pub = is_new_start(REMOTE_PUBLISH_INTERVAL)
+gen_remote_backup = is_new_start(REMOTE_BACKUP_INTERVAL)
+
+
+def main():
+  while True:
+    sleep(0.01)
+    dttm = datetime.utcnow()
+
+    if next(gen_local_pub):
+      data = meassys.data
+
+      if data is None:
+        print(dttm, 'No data.')
+        continue
+
+      data, status, errors = data
+      print(dttm, (data, status, errors))
+
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    meassys.stop()
+    sleep(0.5)
+    print(datetime.utcnow(), 'Interrupted')
+    try:
+      sys.exit(0)
+    except SystemExit:
+      os._exit(0)
