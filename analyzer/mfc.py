980be3750239232825b9c17a0255d71f22b786ac
Yusheng <wuyusheng1023@gmail.com>
2021-04-21 12:49:26 +0100

    mfc get set

--- analyzer/mfc.py
+++ analyzer/mfc.py
@@ -7,15 +7,25 @@ import adafruit_mcp4725
 
 class Mfc():
 
-  def __init__(self, dac, adc):
+  def __init__(self, dac, adc, flow_range, raw_range=4095, volt_range=5.0):
     self.dac = dac
-    self.dac.normalized_value = 1.0
     self.adc = adc
+    self.flow_range = flow_range
+    self.raw_range = raw_range
+    self.volt_range = volt_range
+    self.dac.normalized_value = 1.0
 
   def __str__(self):
     return
 
+  def set(self, value):
+    value = value if value <= self.flow_range else self.flow_range
+    self.dac.raw_value = int(self.raw_range * value / self.flow_range)
   
+  @property
+  def flow(self):
+    return self.adc.voltage / self.volt_range * self.flow_range
+
 
 if __name__ == '__main__':
   i2c = busio.I2C(board.SCL, board.SDA)
@@ -23,7 +33,6 @@ if __name__ == '__main__':
   ads = ADS.ADS1015(i2c)
   adc = AnalogIn(ads, ADS.P1)
 
-  raw_range = 4095
-  mfc = Mfc(dac, adc)
-  mfc.dac.raw_value = int(0.5 * raw_range)
-  print(mfc.adc.voltage)
+  mfc = Mfc(dac, adc, 3)
+  mfc.set(2)
+  print(mfc.flow)
