b93acc01fdda2dc711fb048fd25bab6b10c34396
Yusheng <wuyusheng1023@gmail.com>
2021-04-24 11:26:26 +0100

    move fetch data logic to App from DataTable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'antd/dist/antd.css'
 import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
 
-
+import url from './conf.js';
+import useInterval from './hooks/useInterval.jsx';
+import apiGet from './helpers/apiGet.js';
 import MainSwitch from './components/MainSwitch.jsx';
 import DataTable from './components/DataTable.jsx';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Row style={{paddingTop: 20}}>
-          <Col style={{padding: 10}} span={8} offset={0}>
-            <Row>
-              <MainSwitch />
-            </Row>
-            <Row>
-              <DataTable />
-            </Row>
-          </Col>
-          <Col style={{padding: 10}} span={16} offset={0}>
-            col-6 col-offset-6
-          </Col>
-        </Row>
-      </div>
-    );
-  };
-};
+export default function App () {
 
-export default App;
+  const urlData = url + 'data';
+  const [data, setData] = useState(null);
+
+  const dataProcessor = data => {
+    setData(data);
+    return data['status'];
+  };
+
+  useInterval(() => {
+    apiGet(urlData, dataProcessor);
+    setTimeout(() => {}, 100);
+  }, 500);
+
+  return (
+    <div className="App">
+      <Row style={{paddingTop: 20}}>
+        <Col style={{padding: 10}} span={8} offset={0}>
+          <Row>
+            <MainSwitch />
+          </Row>
+          <Row>
+            <DataTable />
+          </Row>
+        </Col>
+        <Col style={{padding: 10}} span={16} offset={0}>
+          <h1>Plots</h1>
+        </Col>
+      </Row>
+    </div>
+  );
+};
--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -1,23 +1,6 @@
-import { useState } from 'react';
-
-import url from '../conf.js';
-import useInterval from '../hooks/useInterval.jsx';
-import apiGet from '../helpers/apiGet.js';
+import React from 'react';
 
 
 export default function DataTable() {
-  const urlData = url + 'data';
-  const [data, setData] = useState(null);
-
-  const dataProcessor = data => {
-    setData(data);
-    return data['status'];
-  };
-
-  useInterval(() => {
-    apiGet(urlData, dataProcessor);
-    setTimeout(() => {}, 100);
-  }, 500);
-  
-  return 'data table';
+  return <h1>data table</h1>;
 };
